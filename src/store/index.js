732681288f8d78badd45cc5897d277f56f398457
Andrey Ziuzin <zyzin@hotmail.com>
2021-04-14 13:34:43 +0300

    base setup is done

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,15 @@
+"use strict";
+
 import { createStore } from "vuex";
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state() {
+    return {};
+  },
+  mutations,
+  actions,
+  getters,
 });
